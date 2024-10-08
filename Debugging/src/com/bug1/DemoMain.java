5c167ed6ca777298ac519e498f40f9ae5d77921c
potharajuprashanth <potharajuprashanth952@gmail.com>
2024-10-08 14:25:56 +0530

    Add files via upload

---
+++ Debugging/src/com/bug1/DemoMain.java
@@ -0,0 +1,14 @@
+package com.bug1;
+
+public class DemoMain {
+
+	public static void main(String[] args) {
+		System.out.println("started jvm......");
+		Multiplication ob=new Multiplication();
+		System.out.println("After object creation...");
+		ob.cal(12,0);
+		System.out.println(ob);
+
+	}
+
+}
---
+++ Debugging/src/com/bug1/Multiplication.java
@@ -0,0 +1,9 @@
+package com.bug1;
+
+public class Multiplication {
+double cal(int x,int y)
+{
+	double mul=x/y;
+	return mul;
+}
+}
